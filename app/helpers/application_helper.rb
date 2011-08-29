668a4ab56acc7214f0e179e32c3416602c2e2960
Thomas Chen <foxnewsnetwork@gmail.com>
2011-08-29 15:12:01 -0700

    Added style sheets and other things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# Deals with the title for a page to be honest, I have no clue how it works
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if @title.nil?
+			return base_title
+		else
+			return "#{base_title} | #{@title}"
+		end
+	end
 end
