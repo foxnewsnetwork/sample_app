ca94733eab631941dc2043a1393604bbeb77d8c3
Thomas Chen <foxnewsnetwork@gmail.com>
2011-08-30 23:03:53 -0700

    added styles and removed default rails page

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  describe "GET /layout_links" do
+    it "works! (now write some real specs)" do
+      get layout_links_path
+    end
+  end
+  
+  it "should have a home page at '/'" do
+    get '/contact'
+    response.should have_selector( 'title' , :content => 'Contact' )
+  end
+  
+  it "should have an about page at '/about'" do
+    get '/about'
+    response.should have_selector( 'title' , :content => 'About' )
+  end
+  
+  it "should have a help page at '/help'" do
+    get '/help'
+    response.should have_selector( 'title' , :content => 'Help' )
+  end
+  
+  it "should have a contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector( 'title' , :content => 'Contact' )
+end
